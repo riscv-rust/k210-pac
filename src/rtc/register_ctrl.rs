4bee207f3d57cd9abf38f6e248f5c4412a03cfeb
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2019-04-17 17:31:32 +0300

    rtc

---
+++ src/rtc/register_ctrl.rs
@@ -0,0 +1,382 @@
+#[doc = r" Value read from the register"]
+pub struct R {
+    bits: u32,
+}
+#[doc = r" Value to write to the register"]
+pub struct W {
+    bits: u32,
+}
+impl super::REGISTER_CTRL {
+    #[doc = r" Modifies the contents of the register"]
+    #[inline]
+    pub fn modify<F>(&self, f: F)
+    where
+        for<'w> F: FnOnce(&R, &'w mut W) -> &'w mut W,
+    {
+        let bits = self.register.get();
+        let r = R { bits: bits };
+        let mut w = W { bits: bits };
+        f(&r, &mut w);
+        self.register.set(w.bits);
+    }
+    #[doc = r" Reads the contents of the register"]
+    #[inline]
+    pub fn read(&self) -> R {
+        R {
+            bits: self.register.get(),
+        }
+    }
+    #[doc = r" Writes to the register"]
+    #[inline]
+    pub fn write<F>(&self, f: F)
+    where
+        F: FnOnce(&mut W) -> &mut W,
+    {
+        let mut w = W::reset_value();
+        f(&mut w);
+        self.register.set(w.bits);
+    }
+    #[doc = r" Writes the reset value to the register"]
+    #[inline]
+    pub fn reset(&self) {
+        self.write(|w| w)
+    }
+}
+#[doc = r" Value of the field"]
+pub struct READ_ENABLER {
+    bits: bool,
+}
+impl READ_ENABLER {
+    #[doc = r" Value of the field as raw bits"]
+    #[inline]
+    pub fn bit(&self) -> bool {
+        self.bits
+    }
+    #[doc = r" Returns `true` if the bit is clear (0)"]
+    #[inline]
+    pub fn bit_is_clear(&self) -> bool {
+        !self.bit()
+    }
+    #[doc = r" Returns `true` if the bit is set (1)"]
+    #[inline]
+    pub fn bit_is_set(&self) -> bool {
+        self.bit()
+    }
+}
+#[doc = r" Value of the field"]
+pub struct WRITE_ENABLER {
+    bits: bool,
+}
+impl WRITE_ENABLER {
+    #[doc = r" Value of the field as raw bits"]
+    #[inline]
+    pub fn bit(&self) -> bool {
+        self.bits
+    }
+    #[doc = r" Returns `true` if the bit is clear (0)"]
+    #[inline]
+    pub fn bit_is_clear(&self) -> bool {
+        !self.bit()
+    }
+    #[doc = r" Returns `true` if the bit is set (1)"]
+    #[inline]
+    pub fn bit_is_set(&self) -> bool {
+        self.bit()
+    }
+}
+#[doc = r" Value of the field"]
+pub struct TIMER_MASKR {
+    bits: u8,
+}
+impl TIMER_MASKR {
+    #[doc = r" Value of the field as raw bits"]
+    #[inline]
+    pub fn bits(&self) -> u8 {
+        self.bits
+    }
+}
+#[doc = r" Value of the field"]
+pub struct ALARM_MASKR {
+    bits: u8,
+}
+impl ALARM_MASKR {
+    #[doc = r" Value of the field as raw bits"]
+    #[inline]
+    pub fn bits(&self) -> u8 {
+        self.bits
+    }
+}
+#[doc = r" Value of the field"]
+pub struct INITIAL_COUNT_MASKR {
+    bits: bool,
+}
+impl INITIAL_COUNT_MASKR {
+    #[doc = r" Value of the field as raw bits"]
+    #[inline]
+    pub fn bit(&self) -> bool {
+        self.bits
+    }
+    #[doc = r" Returns `true` if the bit is clear (0)"]
+    #[inline]
+    pub fn bit_is_clear(&self) -> bool {
+        !self.bit()
+    }
+    #[doc = r" Returns `true` if the bit is set (1)"]
+    #[inline]
+    pub fn bit_is_set(&self) -> bool {
+        self.bit()
+    }
+}
+#[doc = r" Value of the field"]
+pub struct INTERRUPT_REGISTER_MASKR {
+    bits: bool,
+}
+impl INTERRUPT_REGISTER_MASKR {
+    #[doc = r" Value of the field as raw bits"]
+    #[inline]
+    pub fn bit(&self) -> bool {
+        self.bits
+    }
+    #[doc = r" Returns `true` if the bit is clear (0)"]
+    #[inline]
+    pub fn bit_is_clear(&self) -> bool {
+        !self.bit()
+    }
+    #[doc = r" Returns `true` if the bit is set (1)"]
+    #[inline]
+    pub fn bit_is_set(&self) -> bool {
+        self.bit()
+    }
+}
+#[doc = r" Proxy"]
+pub struct _READ_ENABLEW<'a> {
+    w: &'a mut W,
+}
+impl<'a> _READ_ENABLEW<'a> {
+    #[doc = r" Sets the field bit"]
+    pub fn set_bit(self) -> &'a mut W {
+        self.bit(true)
+    }
+    #[doc = r" Clears the field bit"]
+    pub fn clear_bit(self) -> &'a mut W {
+        self.bit(false)
+    }
+    #[doc = r" Writes raw bits to the field"]
+    #[inline]
+    pub fn bit(self, value: bool) -> &'a mut W {
+        const MASK: bool = true;
+        const OFFSET: u8 = 0;
+        self.w.bits &= !((MASK as u32) << OFFSET);
+        self.w.bits |= ((value & MASK) as u32) << OFFSET;
+        self.w
+    }
+}
+#[doc = r" Proxy"]
+pub struct _WRITE_ENABLEW<'a> {
+    w: &'a mut W,
+}
+impl<'a> _WRITE_ENABLEW<'a> {
+    #[doc = r" Sets the field bit"]
+    pub fn set_bit(self) -> &'a mut W {
+        self.bit(true)
+    }
+    #[doc = r" Clears the field bit"]
+    pub fn clear_bit(self) -> &'a mut W {
+        self.bit(false)
+    }
+    #[doc = r" Writes raw bits to the field"]
+    #[inline]
+    pub fn bit(self, value: bool) -> &'a mut W {
+        const MASK: bool = true;
+        const OFFSET: u8 = 1;
+        self.w.bits &= !((MASK as u32) << OFFSET);
+        self.w.bits |= ((value & MASK) as u32) << OFFSET;
+        self.w
+    }
+}
+#[doc = r" Proxy"]
+pub struct _TIMER_MASKW<'a> {
+    w: &'a mut W,
+}
+impl<'a> _TIMER_MASKW<'a> {
+    #[doc = r" Writes raw bits to the field"]
+    #[inline]
+    pub unsafe fn bits(self, value: u8) -> &'a mut W {
+        const MASK: u8 = 255;
+        const OFFSET: u8 = 13;
+        self.w.bits &= !((MASK as u32) << OFFSET);
+        self.w.bits |= ((value & MASK) as u32) << OFFSET;
+        self.w
+    }
+}
+#[doc = r" Proxy"]
+pub struct _ALARM_MASKW<'a> {
+    w: &'a mut W,
+}
+impl<'a> _ALARM_MASKW<'a> {
+    #[doc = r" Writes raw bits to the field"]
+    #[inline]
+    pub unsafe fn bits(self, value: u8) -> &'a mut W {
+        const MASK: u8 = 255;
+        const OFFSET: u8 = 21;
+        self.w.bits &= !((MASK as u32) << OFFSET);
+        self.w.bits |= ((value & MASK) as u32) << OFFSET;
+        self.w
+    }
+}
+#[doc = r" Proxy"]
+pub struct _INITIAL_COUNT_MASKW<'a> {
+    w: &'a mut W,
+}
+impl<'a> _INITIAL_COUNT_MASKW<'a> {
+    #[doc = r" Sets the field bit"]
+    pub fn set_bit(self) -> &'a mut W {
+        self.bit(true)
+    }
+    #[doc = r" Clears the field bit"]
+    pub fn clear_bit(self) -> &'a mut W {
+        self.bit(false)
+    }
+    #[doc = r" Writes raw bits to the field"]
+    #[inline]
+    pub fn bit(self, value: bool) -> &'a mut W {
+        const MASK: bool = true;
+        const OFFSET: u8 = 29;
+        self.w.bits &= !((MASK as u32) << OFFSET);
+        self.w.bits |= ((value & MASK) as u32) << OFFSET;
+        self.w
+    }
+}
+#[doc = r" Proxy"]
+pub struct _INTERRUPT_REGISTER_MASKW<'a> {
+    w: &'a mut W,
+}
+impl<'a> _INTERRUPT_REGISTER_MASKW<'a> {
+    #[doc = r" Sets the field bit"]
+    pub fn set_bit(self) -> &'a mut W {
+        self.bit(true)
+    }
+    #[doc = r" Clears the field bit"]
+    pub fn clear_bit(self) -> &'a mut W {
+        self.bit(false)
+    }
+    #[doc = r" Writes raw bits to the field"]
+    #[inline]
+    pub fn bit(self, value: bool) -> &'a mut W {
+        const MASK: bool = true;
+        const OFFSET: u8 = 30;
+        self.w.bits &= !((MASK as u32) << OFFSET);
+        self.w.bits |= ((value & MASK) as u32) << OFFSET;
+        self.w
+    }
+}
+impl R {
+    #[doc = r" Value of the register as raw bits"]
+    #[inline]
+    pub fn bits(&self) -> u32 {
+        self.bits
+    }
+    #[doc = "Bit 0 - RTC timer read enable"]
+    #[inline]
+    pub fn read_enable(&self) -> READ_ENABLER {
+        let bits = {
+            const MASK: bool = true;
+            const OFFSET: u8 = 0;
+            ((self.bits >> OFFSET) & MASK as u32) != 0
+        };
+        READ_ENABLER { bits }
+    }
+    #[doc = "Bit 1 - RTC timer write enable"]
+    #[inline]
+    pub fn write_enable(&self) -> WRITE_ENABLER {
+        let bits = {
+            const MASK: bool = true;
+            const OFFSET: u8 = 1;
+            ((self.bits >> OFFSET) & MASK as u32) != 0
+        };
+        WRITE_ENABLER { bits }
+    }
+    #[doc = "Bits 13:20 - RTC timer mask"]
+    #[inline]
+    pub fn timer_mask(&self) -> TIMER_MASKR {
+        let bits = {
+            const MASK: u8 = 255;
+            const OFFSET: u8 = 13;
+            ((self.bits >> OFFSET) & MASK as u32) as u8
+        };
+        TIMER_MASKR { bits }
+    }
+    #[doc = "Bits 21:28 - RTC alarm mask"]
+    #[inline]
+    pub fn alarm_mask(&self) -> ALARM_MASKR {
+        let bits = {
+            const MASK: u8 = 255;
+            const OFFSET: u8 = 21;
+            ((self.bits >> OFFSET) & MASK as u32) as u8
+        };
+        ALARM_MASKR { bits }
+    }
+    #[doc = "Bit 29 - RTC counter initial count value mask"]
+    #[inline]
+    pub fn initial_count_mask(&self) -> INITIAL_COUNT_MASKR {
+        let bits = {
+            const MASK: bool = true;
+            const OFFSET: u8 = 29;
+            ((self.bits >> OFFSET) & MASK as u32) != 0
+        };
+        INITIAL_COUNT_MASKR { bits }
+    }
+    #[doc = "Bit 30 - RTC interrupt register mask"]
+    #[inline]
+    pub fn interrupt_register_mask(&self) -> INTERRUPT_REGISTER_MASKR {
+        let bits = {
+            const MASK: bool = true;
+            const OFFSET: u8 = 30;
+            ((self.bits >> OFFSET) & MASK as u32) != 0
+        };
+        INTERRUPT_REGISTER_MASKR { bits }
+    }
+}
+impl W {
+    #[doc = r" Reset value of the register"]
+    #[inline]
+    pub fn reset_value() -> W {
+        W { bits: 0 }
+    }
+    #[doc = r" Writes raw bits to the register"]
+    #[inline]
+    pub unsafe fn bits(&mut self, bits: u32) -> &mut Self {
+        self.bits = bits;
+        self
+    }
+    #[doc = "Bit 0 - RTC timer read enable"]
+    #[inline]
+    pub fn read_enable(&mut self) -> _READ_ENABLEW {
+        _READ_ENABLEW { w: self }
+    }
+    #[doc = "Bit 1 - RTC timer write enable"]
+    #[inline]
+    pub fn write_enable(&mut self) -> _WRITE_ENABLEW {
+        _WRITE_ENABLEW { w: self }
+    }
+    #[doc = "Bits 13:20 - RTC timer mask"]
+    #[inline]
+    pub fn timer_mask(&mut self) -> _TIMER_MASKW {
+        _TIMER_MASKW { w: self }
+    }
+    #[doc = "Bits 21:28 - RTC alarm mask"]
+    #[inline]
+    pub fn alarm_mask(&mut self) -> _ALARM_MASKW {
+        _ALARM_MASKW { w: self }
+    }
+    #[doc = "Bit 29 - RTC counter initial count value mask"]
+    #[inline]
+    pub fn initial_count_mask(&mut self) -> _INITIAL_COUNT_MASKW {
+        _INITIAL_COUNT_MASKW { w: self }
+    }
+    #[doc = "Bit 30 - RTC interrupt register mask"]
+    #[inline]
+    pub fn interrupt_register_mask(&mut self) -> _INTERRUPT_REGISTER_MASKW {
+        _INTERRUPT_REGISTER_MASKW { w: self }
+    }
+}
