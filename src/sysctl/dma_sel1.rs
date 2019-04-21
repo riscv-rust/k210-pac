15ea42c3003cb4447cb5eff0042c4aab583434b1
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2019-04-21 13:13:16 +0300

    sysctl_dma_select

--- src/sysctl/dma_sel1.rs
+++ src/sysctl/dma_sel1.rs
@@ -42,22 +42,185 @@ impl super::DMA_SEL1 {
         self.write(|w| w)
     }
 }
-#[doc = r" Value of the field"]
-pub struct DMA_SEL5R {
-    bits: u8,
-}
-impl DMA_SEL5R {
-    #[doc = r" Value of the field as raw bits"]
-    #[inline]
-    pub fn bits(&self) -> u8 {
-        self.bits
-    }
-}
+#[doc = "Possible values of the field `dma_sel5`"]
+pub type DMA_SEL5R = super::dma_sel0::DMA_SEL0R;
+#[doc = "Values that can be written to the field `dma_sel5`"]
+pub type DMA_SEL5W = super::dma_sel0::DMA_SEL0W;
 #[doc = r" Proxy"]
 pub struct _DMA_SEL5W<'a> {
     w: &'a mut W,
 }
 impl<'a> _DMA_SEL5W<'a> {
+    #[doc = r" Writes `variant` to the field"]
+    #[inline]
+    pub fn variant(self, variant: DMA_SEL5W) -> &'a mut W {
+        unsafe { self.bits(variant._bits()) }
+    }
+    #[doc = "`0`"]
+    #[inline]
+    pub fn ssi0_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::SSI0_RX_REQ)
+    }
+    #[doc = "`1`"]
+    #[inline]
+    pub fn ssi0_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::SSI0_TX_REQ)
+    }
+    #[doc = "`10`"]
+    #[inline]
+    pub fn ssi1_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::SSI1_RX_REQ)
+    }
+    #[doc = "`11`"]
+    #[inline]
+    pub fn ssi1_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::SSI1_TX_REQ)
+    }
+    #[doc = "`100`"]
+    #[inline]
+    pub fn ssi2_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::SSI2_RX_REQ)
+    }
+    #[doc = "`101`"]
+    #[inline]
+    pub fn ssi2_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::SSI2_TX_REQ)
+    }
+    #[doc = "`110`"]
+    #[inline]
+    pub fn ssi3_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::SSI3_RX_REQ)
+    }
+    #[doc = "`111`"]
+    #[inline]
+    pub fn ssi3_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::SSI3_TX_REQ)
+    }
+    #[doc = "`1000`"]
+    #[inline]
+    pub fn i2c0_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2C0_RX_REQ)
+    }
+    #[doc = "`1001`"]
+    #[inline]
+    pub fn i2c0_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2C0_TX_REQ)
+    }
+    #[doc = "`1010`"]
+    #[inline]
+    pub fn i2c1_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2C1_RX_REQ)
+    }
+    #[doc = "`1011`"]
+    #[inline]
+    pub fn i2c1_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2C1_TX_REQ)
+    }
+    #[doc = "`1100`"]
+    #[inline]
+    pub fn i2c2_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2C2_RX_REQ)
+    }
+    #[doc = "`1101`"]
+    #[inline]
+    pub fn i2c2_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2C2_TX_REQ)
+    }
+    #[doc = "`1110`"]
+    #[inline]
+    pub fn uart1_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::UART1_RX_REQ)
+    }
+    #[doc = "`1111`"]
+    #[inline]
+    pub fn uart1_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::UART1_TX_REQ)
+    }
+    #[doc = "`10000`"]
+    #[inline]
+    pub fn uart2_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::UART2_RX_REQ)
+    }
+    #[doc = "`10001`"]
+    #[inline]
+    pub fn uart2_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::UART2_TX_REQ)
+    }
+    #[doc = "`10010`"]
+    #[inline]
+    pub fn uart3_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::UART3_RX_REQ)
+    }
+    #[doc = "`10011`"]
+    #[inline]
+    pub fn uart3_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::UART3_TX_REQ)
+    }
+    #[doc = "`10100`"]
+    #[inline]
+    pub fn aes_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::AES_REQ)
+    }
+    #[doc = "`10101`"]
+    #[inline]
+    pub fn sha_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::SHA_RX_REQ)
+    }
+    #[doc = "`10110`"]
+    #[inline]
+    pub fn ai_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::AI_RX_REQ)
+    }
+    #[doc = "`10111`"]
+    #[inline]
+    pub fn fft_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::FFT_RX_REQ)
+    }
+    #[doc = "`11000`"]
+    #[inline]
+    pub fn fft_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::FFT_TX_REQ)
+    }
+    #[doc = "`11001`"]
+    #[inline]
+    pub fn i2s0_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2S0_TX_REQ)
+    }
+    #[doc = "`11010`"]
+    #[inline]
+    pub fn i2s0_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2S0_RX_REQ)
+    }
+    #[doc = "`11011`"]
+    #[inline]
+    pub fn i2s1_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2S1_TX_REQ)
+    }
+    #[doc = "`11100`"]
+    #[inline]
+    pub fn i2s1_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2S1_RX_REQ)
+    }
+    #[doc = "`11101`"]
+    #[inline]
+    pub fn i2s2_tx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2S2_TX_REQ)
+    }
+    #[doc = "`11110`"]
+    #[inline]
+    pub fn i2s2_rx_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2S2_RX_REQ)
+    }
+    #[doc = "`11111`"]
+    #[inline]
+    pub fn i2s0_bf_dir_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2S0_BF_DIR_REQ)
+    }
+    #[doc = "`100000`"]
+    #[inline]
+    pub fn i2s0_bf_voice_req(self) -> &'a mut W {
+        self.variant(super::dma_sel0::DMA_SEL0W::I2S0_BF_VOICE_REQ)
+    }
     #[doc = r" Writes raw bits to the field"]
     #[inline]
     pub unsafe fn bits(self, value: u8) -> &'a mut W {
@@ -77,12 +240,11 @@ impl R {
     #[doc = "Bits 0:5"]
     #[inline]
     pub fn dma_sel5(&self) -> DMA_SEL5R {
-        let bits = {
+        DMA_SEL5R::_from({
             const MASK: u8 = 63;
             const OFFSET: u8 = 0;
             ((self.bits >> OFFSET) & MASK as u32) as u8
-        };
-        DMA_SEL5R { bits }
+        })
     }
 }
 impl W {
