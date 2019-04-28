d0311c97322b99ed7f9122a00632ab129afa32e4
Wladimir J. van der Laan <laanwj@gmail.com>
2019-04-28 22:22:44 +0200

    SHA256
    From `lib/drivers/include/sha256.h` in kendryte sdk.

--- src/sha256.rs
+++ src/sha256.rs
@@ -1,12 +1,46 @@
 #[doc = r" Register block"]
 #[repr(C)]
 pub struct RegisterBlock {
-    #[doc = "0x00 - Dummy register: this peripheral is not implemented yet"]
-    pub dummy: DUMMY,
+    #[doc = "0x00 - Calculated SHA256 return value"]
+    pub result: [RESULT; 8],
+    #[doc = "0x20 - SHA256 input data is written to this register"]
+    pub data_in: DATA_IN,
+    _reserved0: [u8; 4usize],
+    #[doc = "0x28 - Counters register"]
+    pub num_reg: NUM_REG,
+    #[doc = "0x2c - Function configuration register 0"]
+    pub function_reg_0: FUNCTION_REG_0,
+    _reserved1: [u8; 4usize],
+    #[doc = "0x34 - Function configuration register 1"]
+    pub function_reg_1: FUNCTION_REG_1,
 }
-#[doc = "Dummy register: this peripheral is not implemented yet"]
-pub struct DUMMY {
+#[doc = "Calculated SHA256 return value"]
+pub struct RESULT {
     register: ::vcell::VolatileCell<u32>,
 }
-#[doc = "Dummy register: this peripheral is not implemented yet"]
-pub mod dummy;
+#[doc = "Calculated SHA256 return value"]
+pub mod result;
+#[doc = "SHA256 input data is written to this register"]
+pub struct DATA_IN {
+    register: ::vcell::VolatileCell<u32>,
+}
+#[doc = "SHA256 input data is written to this register"]
+pub mod data_in;
+#[doc = "Counters register"]
+pub struct NUM_REG {
+    register: ::vcell::VolatileCell<u32>,
+}
+#[doc = "Counters register"]
+pub mod num_reg;
+#[doc = "Function configuration register 0"]
+pub struct FUNCTION_REG_0 {
+    register: ::vcell::VolatileCell<u32>,
+}
+#[doc = "Function configuration register 0"]
+pub mod function_reg_0;
+#[doc = "Function configuration register 1"]
+pub struct FUNCTION_REG_1 {
+    register: ::vcell::VolatileCell<u32>,
+}
+#[doc = "Function configuration register 1"]
+pub mod function_reg_1;
---
+++ src/sha256/function_reg_0.rs
@@ -0,0 +1,302 @@
+#[doc = r" Value read from the register"]
+pub struct R {
+    bits: u32,
+}
+#[doc = r" Value to write to the register"]
+pub struct W {
+    bits: u32,
+}
+impl super::FUNCTION_REG_0 {
+    #[doc = r" Modifies the contents of the register"]
+    #[inline]
+    pub fn modify<F>(&self, f: F)
+    where
+        for<'w> F: FnOnce(&R, &'w mut W) -> &'w mut W,
+    {
+        let bits = self.register.get();
+        let r = R { bits: bits };
+        let mut w = W { bits: bits };
+        f(&r, &mut w);
+        self.register.set(w.bits);
+    }
+    #[doc = r" Reads the contents of the register"]
+    #[inline]
+    pub fn read(&self) -> R {
+        R {
+            bits: self.register.get(),
+        }
+    }
+    #[doc = r" Writes to the register"]
+    #[inline]
+    pub fn write<F>(&self, f: F)
+    where
+        F: FnOnce(&mut W) -> &mut W,
+    {
+        let mut w = W::reset_value();
+        f(&mut w);
+        self.register.set(w.bits);
+    }
+    #[doc = r" Writes the reset value to the register"]
+    #[inline]
+    pub fn reset(&self) {
+        self.write(|w| w)
+    }
+}
+#[doc = r" Value of the field"]
+pub struct ENR {
+    bits: bool,
+}
+impl ENR {
+    #[doc = r" Value of the field as raw bits"]
+    #[inline]
+    pub fn bit(&self) -> bool {
+        self.bits
+    }
+    #[doc = r" Returns `true` if the bit is clear (0)"]
+    #[inline]
+    pub fn bit_is_clear(&self) -> bool {
+        !self.bit()
+    }
+    #[doc = r" Returns `true` if the bit is set (1)"]
+    #[inline]
+    pub fn bit_is_set(&self) -> bool {
+        self.bit()
+    }
+}
+#[doc = r" Value of the field"]
+pub struct OVERFLOWR {
+    bits: bool,
+}
+impl OVERFLOWR {
+    #[doc = r" Value of the field as raw bits"]
+    #[inline]
+    pub fn bit(&self) -> bool {
+        self.bits
+    }
+    #[doc = r" Returns `true` if the bit is clear (0)"]
+    #[inline]
+    pub fn bit_is_clear(&self) -> bool {
+        !self.bit()
+    }
+    #[doc = r" Returns `true` if the bit is set (1)"]
+    #[inline]
+    pub fn bit_is_set(&self) -> bool {
+        self.bit()
+    }
+}
+#[doc = "Possible values of the field `endian`"]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ENDIANR {
+    #[doc = "Little endian"]
+    LE,
+    #[doc = "Big endian"]
+    BE,
+}
+impl ENDIANR {
+    #[doc = r" Returns `true` if the bit is clear (0)"]
+    #[inline]
+    pub fn bit_is_clear(&self) -> bool {
+        !self.bit()
+    }
+    #[doc = r" Returns `true` if the bit is set (1)"]
+    #[inline]
+    pub fn bit_is_set(&self) -> bool {
+        self.bit()
+    }
+    #[doc = r" Value of the field as raw bits"]
+    #[inline]
+    pub fn bit(&self) -> bool {
+        match *self {
+            ENDIANR::LE => false,
+            ENDIANR::BE => true,
+        }
+    }
+    #[allow(missing_docs)]
+    #[doc(hidden)]
+    #[inline]
+    pub fn _from(value: bool) -> ENDIANR {
+        match value {
+            false => ENDIANR::LE,
+            true => ENDIANR::BE,
+        }
+    }
+    #[doc = "Checks if the value of the field is `LE`"]
+    #[inline]
+    pub fn is_le(&self) -> bool {
+        *self == ENDIANR::LE
+    }
+    #[doc = "Checks if the value of the field is `BE`"]
+    #[inline]
+    pub fn is_be(&self) -> bool {
+        *self == ENDIANR::BE
+    }
+}
+#[doc = r" Proxy"]
+pub struct _ENW<'a> {
+    w: &'a mut W,
+}
+impl<'a> _ENW<'a> {
+    #[doc = r" Sets the field bit"]
+    pub fn set_bit(self) -> &'a mut W {
+        self.bit(true)
+    }
+    #[doc = r" Clears the field bit"]
+    pub fn clear_bit(self) -> &'a mut W {
+        self.bit(false)
+    }
+    #[doc = r" Writes raw bits to the field"]
+    #[inline]
+    pub fn bit(self, value: bool) -> &'a mut W {
+        const MASK: bool = true;
+        const OFFSET: u8 = 0;
+        self.w.bits &= !((MASK as u32) << OFFSET);
+        self.w.bits |= ((value & MASK) as u32) << OFFSET;
+        self.w
+    }
+}
+#[doc = r" Proxy"]
+pub struct _OVERFLOWW<'a> {
+    w: &'a mut W,
+}
+impl<'a> _OVERFLOWW<'a> {
+    #[doc = r" Sets the field bit"]
+    pub fn set_bit(self) -> &'a mut W {
+        self.bit(true)
+    }
+    #[doc = r" Clears the field bit"]
+    pub fn clear_bit(self) -> &'a mut W {
+        self.bit(false)
+    }
+    #[doc = r" Writes raw bits to the field"]
+    #[inline]
+    pub fn bit(self, value: bool) -> &'a mut W {
+        const MASK: bool = true;
+        const OFFSET: u8 = 8;
+        self.w.bits &= !((MASK as u32) << OFFSET);
+        self.w.bits |= ((value & MASK) as u32) << OFFSET;
+        self.w
+    }
+}
+#[doc = "Values that can be written to the field `endian`"]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ENDIANW {
+    #[doc = "Little endian"]
+    LE,
+    #[doc = "Big endian"]
+    BE,
+}
+impl ENDIANW {
+    #[allow(missing_docs)]
+    #[doc(hidden)]
+    #[inline]
+    pub fn _bits(&self) -> bool {
+        match *self {
+            ENDIANW::LE => false,
+            ENDIANW::BE => true,
+        }
+    }
+}
+#[doc = r" Proxy"]
+pub struct _ENDIANW<'a> {
+    w: &'a mut W,
+}
+impl<'a> _ENDIANW<'a> {
+    #[doc = r" Writes `variant` to the field"]
+    #[inline]
+    pub fn variant(self, variant: ENDIANW) -> &'a mut W {
+        {
+            self.bit(variant._bits())
+        }
+    }
+    #[doc = "Little endian"]
+    #[inline]
+    pub fn le(self) -> &'a mut W {
+        self.variant(ENDIANW::LE)
+    }
+    #[doc = "Big endian"]
+    #[inline]
+    pub fn be(self) -> &'a mut W {
+        self.variant(ENDIANW::BE)
+    }
+    #[doc = r" Sets the field bit"]
+    pub fn set_bit(self) -> &'a mut W {
+        self.bit(true)
+    }
+    #[doc = r" Clears the field bit"]
+    pub fn clear_bit(self) -> &'a mut W {
+        self.bit(false)
+    }
+    #[doc = r" Writes raw bits to the field"]
+    #[inline]
+    pub fn bit(self, value: bool) -> &'a mut W {
+        const MASK: bool = true;
+        const OFFSET: u8 = 16;
+        self.w.bits &= !((MASK as u32) << OFFSET);
+        self.w.bits |= ((value & MASK) as u32) << OFFSET;
+        self.w
+    }
+}
+impl R {
+    #[doc = r" Value of the register as raw bits"]
+    #[inline]
+    pub fn bits(&self) -> u32 {
+        self.bits
+    }
+    #[doc = "Bit 0 - write:SHA256 enable register. read:Calculation completed flag"]
+    #[inline]
+    pub fn en(&self) -> ENR {
+        let bits = {
+            const MASK: bool = true;
+            const OFFSET: u8 = 0;
+            ((self.bits >> OFFSET) & MASK as u32) != 0
+        };
+        ENR { bits }
+    }
+    #[doc = "Bit 8 - SHA256 calculation overflow flag"]
+    #[inline]
+    pub fn overflow(&self) -> OVERFLOWR {
+        let bits = {
+            const MASK: bool = true;
+            const OFFSET: u8 = 8;
+            ((self.bits >> OFFSET) & MASK as u32) != 0
+        };
+        OVERFLOWR { bits }
+    }
+    #[doc = "Bit 16 - Endian setting"]
+    #[inline]
+    pub fn endian(&self) -> ENDIANR {
+        ENDIANR::_from({
+            const MASK: bool = true;
+            const OFFSET: u8 = 16;
+            ((self.bits >> OFFSET) & MASK as u32) != 0
+        })
+    }
+}
+impl W {
+    #[doc = r" Reset value of the register"]
+    #[inline]
+    pub fn reset_value() -> W {
+        W { bits: 0 }
+    }
+    #[doc = r" Writes raw bits to the register"]
+    #[inline]
+    pub unsafe fn bits(&mut self, bits: u32) -> &mut Self {
+        self.bits = bits;
+        self
+    }
+    #[doc = "Bit 0 - write:SHA256 enable register. read:Calculation completed flag"]
+    #[inline]
+    pub fn en(&mut self) -> _ENW {
+        _ENW { w: self }
+    }
+    #[doc = "Bit 8 - SHA256 calculation overflow flag"]
+    #[inline]
+    pub fn overflow(&mut self) -> _OVERFLOWW {
+        _OVERFLOWW { w: self }
+    }
+    #[doc = "Bit 16 - Endian setting"]
+    #[inline]
+    pub fn endian(&mut self) -> _ENDIANW {
+        _ENDIANW { w: self }
+    }
+}
